696c470c0067be3dd22795a6d3c587fdc48a44e0
Lucía Pérez Villegas <luciajmnzvg2003@gmail.com>
2025-03-08 18:24:27 +0100

    Clase HealthCalcTest completa

--- project-healthcalc/src/test/java/healthcalc/HealthCalcTest.java
+++ project-healthcalc/src/test/java/healthcalc/HealthCalcTest.java
@@ -15,9 +15,12 @@ public class HealthCalcTest {
 	public void testInvalidHeightException(){
 		// Arrange:
 		HealthCalcImpl c = new HealthCalcImpl();
-		// Act & Assert:
+		// Act & Assert (idealBodyWeight):
 		assertThrows(IllegalArgumentException.class, () -> c.idealBodyWeight(20, 'M'));
 		assertThrows(IllegalArgumentException.class, () -> c.idealBodyWeight(270, 'M'));
+		// Act & Assert (basalMetabolicRate):
+		assertThrows(IllegalArgumentException.class, () -> c.basalMetabolicRate(70, 20, 25, 'M'));
+		assertThrows(IllegalArgumentException.class, () -> c.basalMetabolicRate(70, 270, 25, 'M'));
 	}
 
 	@Test
@@ -25,10 +28,14 @@ public class HealthCalcTest {
 	public void testValidHeightException(){
 		// Arrange:
 		HealthCalcImpl c = new HealthCalcImpl();
-		// Act & Assert:
+		// Act & Assert (idealBodyWeight):
 		assertDoesNotThrow(() -> c.idealBodyWeight(30, 'M'));
 		assertDoesNotThrow(() -> c.idealBodyWeight(170, 'M'));
 		assertDoesNotThrow(() -> c.idealBodyWeight(250, 'M'));
+		// Act & Assert (basalMetabolicRate):
+		assertDoesNotThrow(() -> c.basalMetabolicRate(70, 30, 25, 'M'));
+		assertDoesNotThrow(() -> c.basalMetabolicRate(70, 170, 25, 'M'));
+		assertDoesNotThrow(() -> c.basalMetabolicRate(70, 250, 25, 'M'));
 	}
 
 	@Test
@@ -36,11 +43,69 @@ public class HealthCalcTest {
 	public void testInvalidGender(){
 		// Arrange:
 		HealthCalcImpl c = new HealthCalcImpl();
-		// Act & Assert:
+		// Act & Assert (idealBodyWeight):
 		assertThrows(IllegalArgumentException.class, () -> c.idealBodyWeight(170, 'A'));
 		assertThrows(IllegalArgumentException.class, () -> c.idealBodyWeight(170, 'B'));
 		assertThrows(IllegalArgumentException.class, () -> c.idealBodyWeight(170, 'm'));
 		assertThrows(IllegalArgumentException.class, () -> c.idealBodyWeight(170, 'w'));
+		// Act & Assert (basalMetabolicRate):
+		assertThrows(IllegalArgumentException.class, () -> c.basalMetabolicRate(70, 170, 25, 'A'));
+		assertThrows(IllegalArgumentException.class, () -> c.basalMetabolicRate(70, 170, 25, 'B'));
+		assertThrows(IllegalArgumentException.class, () -> c.basalMetabolicRate(70, 170, 25, 'm'));
+		assertThrows(IllegalArgumentException.class, () -> c.basalMetabolicRate(70, 170, 25, 'w'));
+	}
+
+	@Test
+	@DisplayName("Test de género válido")
+	public void testValidGender(){
+		// Arrange
+		HealthCalcImpl c = new HealthCalcImpl();
+		// Act & Assert (idealBodyWeight):
+		assertDoesNotThrow(() -> c.idealBodyWeight(170, 'M'));
+		assertDoesNotThrow(() -> c.idealBodyWeight(170, 'W'));
+		// Act & Assert (basalMetabolicRate):
+		assertDoesNotThrow(() -> c.basalMetabolicRate(70, 170, 25, 'M'));
+		assertDoesNotThrow(() -> c.basalMetabolicRate(70, 170, 25, 'W'));
+	}
+
+	@Test
+	@DisplayName("Test de peso inválido")
+	public void testInvalidWeight(){
+		// Arrange
+		HealthCalcImpl c = new HealthCalcImpl();
+		// Act & Assert:
+		assertThrows(IllegalArgumentException.class, () -> c.basalMetabolicRate(2, 170, 25, 'M'));
+		assertThrows(IllegalArgumentException.class, () -> c.basalMetabolicRate(450, 170, 25, 'M'));
+	}
+
+	@Test
+	@DisplayName("Test de peso válido")
+	public void testValidWeight(){
+		// Arrange
+		HealthCalcImpl c = new HealthCalcImpl();
+		// Act & Assert:
+		assertDoesNotThrow(()-> c.basalMetabolicRate(20, 170, 25, 'M'));
+		assertDoesNotThrow(()-> c.basalMetabolicRate(150, 170, 25, 'M'));
+	}
+
+	@Test
+	@DisplayName("Test de edad inválida")
+	public void testInvalidAge(){
+		// Arrange
+		HealthCalcImpl c = new HealthCalcImpl();
+		// Act & Assert:
+		assertThrows(IllegalArgumentException.class, () -> c.basalMetabolicRate(70, 170, 0, 'M'));
+		assertThrows(IllegalArgumentException.class, () -> c.basalMetabolicRate(70, 170, 130, 'M'));
+	}
+
+	@Test
+	@DisplayName("Test de edad válida")
+	public void testValidAge(){
+		// Arrange
+		HealthCalcImpl c = new HealthCalcImpl();
+		// Act & Assert:
+		assertDoesNotThrow(() -> c.basalMetabolicRate(70, 170, 25, 'M'));
+		assertDoesNotThrow(() -> c.basalMetabolicRate(70, 170, 50, 'M'));
 	}
 
 }
