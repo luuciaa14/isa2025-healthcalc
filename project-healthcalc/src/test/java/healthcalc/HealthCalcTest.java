b3f85874f0a21851f2daa4911b0986ead2870dc1
Lucía Pérez Villegas <luciajmnzvg2003@gmail.com>
2025-03-25 13:32:14 +0100

    Arreglo clase HealthCalcTest

--- project-healthcalc/src/test/java/healthcalc/HealthCalcTest.java
+++ project-healthcalc/src/test/java/healthcalc/HealthCalcTest.java
@@ -1,7 +1,7 @@
 package healthcalc;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-// import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.DisplayName;
