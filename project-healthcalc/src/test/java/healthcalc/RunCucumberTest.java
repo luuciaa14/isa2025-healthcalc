804987d2758360bc54c809f21f7af337412eca96
Lucía Pérez Villegas <luciajmnzvg2003@gmail.com>
2025-03-25 13:29:06 +0100

    Instalación cucumber

---
+++ project-healthcalc/src/test/java/healthcalc/RunCucumberTest.java
@@ -0,0 +1,10 @@
+package healthcalc;
+
+import org.junit.platform.suite.api.*;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = "cucumber.glue", value = "healthcalc")
+public class RunCucumberTest {
+}
