9d3df9e8db451697de71255b133e3f6c8c709d73
Lucía Pérez Villegas <luciajmnzvg2003@gmail.com>
2025-04-03 20:11:50 +0200

    Añadida clase Vista.java

---
+++ project-healthcalc/src/main/java/healthcalc/Vista.java
@@ -0,0 +1,186 @@
+package healthcalc;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextArea;
+import java.awt.Label;
+import java.awt.TextField;
+import java.awt.Checkbox;
+import java.awt.Rectangle;
+import java.awt.Point;
+import javax.swing.JLabel;
+import java.awt.Component;
+import java.awt.Font;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+public class Vista extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTextField tfAltura;
+	private JTextField tfPeso;
+	private JTextField tfEdad;
+	private JTextField tfPesoIdeal;
+	private JTextField tfTasaMetabolica;
+	private JButton bCalcularPesoIdeal;
+	private JButton bCalcularTasaMetabolica;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Vista frame = new Vista();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Vista() {
+		setTitle("Health calculator");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 624, 396);
+		contentPane = new JPanel();
+		contentPane.setToolTipText("");
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblGenero = new JLabel("Género");
+		lblGenero.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblGenero.setBounds(86, 71, 81, 25);
+		contentPane.add(lblGenero);
+		
+		JButton bMujer = new JButton("Mujer");
+		bMujer.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		bMujer.setBounds(20, 106, 98, 21);
+		contentPane.add(bMujer);
+		
+		JButton bHombre = new JButton("Hombre");
+		bHombre.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		bHombre.setBounds(124, 106, 98, 21);
+		contentPane.add(bHombre);
+		
+		JLabel lblAltura = new JLabel("Altura");
+		lblAltura.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblAltura.setBounds(263, 71, 81, 25);
+		contentPane.add(lblAltura);
+		
+		tfAltura = new JTextField();
+		tfAltura.setHorizontalAlignment(SwingConstants.RIGHT);
+		tfAltura.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		tfAltura.setBounds(247, 107, 50, 19);
+		contentPane.add(tfAltura);
+		tfAltura.setColumns(10);
+		
+		JLabel lblcm1 = new JLabel("cm");
+		lblcm1.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblcm1.setBounds(307, 110, 45, 13);
+		contentPane.add(lblcm1);
+		
+		JLabel lblPeso = new JLabel("Peso");
+		lblPeso.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblPeso.setBounds(376, 71, 81, 25);
+		contentPane.add(lblPeso);
+		
+		tfPeso = new JTextField();
+		tfPeso.setHorizontalAlignment(SwingConstants.RIGHT);
+		tfPeso.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		tfPeso.setBounds(362, 107, 50, 19);
+		contentPane.add(tfPeso);
+		tfPeso.setColumns(10);
+		
+		JLabel lblkg1 = new JLabel("kg");
+		lblkg1.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblkg1.setBounds(422, 104, 45, 25);
+		contentPane.add(lblkg1);
+		
+		JLabel lblEdad = new JLabel("Edad");
+		lblEdad.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblEdad.setBounds(488, 71, 50, 25);
+		contentPane.add(lblEdad);
+		
+		tfEdad = new JTextField();
+		tfEdad.setHorizontalAlignment(SwingConstants.RIGHT);
+		tfEdad.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		tfEdad.setBounds(467, 107, 50, 19);
+		contentPane.add(tfEdad);
+		tfEdad.setColumns(10);
+		
+		JLabel lblAnios = new JLabel("años");
+		lblAnios.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblAnios.setBounds(527, 104, 45, 25);
+		contentPane.add(lblAnios);
+		
+		bCalcularPesoIdeal = new JButton("Peso ideal");
+		bCalcularPesoIdeal.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		bCalcularPesoIdeal.setBounds(73, 192, 149, 25);
+		contentPane.add(bCalcularPesoIdeal);
+		
+		tfPesoIdeal = new JTextField();
+		tfPesoIdeal.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		tfPesoIdeal.setEditable(false);
+		tfPesoIdeal.setBounds(86, 237, 96, 19);
+		contentPane.add(tfPesoIdeal);
+		tfPesoIdeal.setColumns(10);
+		
+		JLabel lblkg2 = new JLabel("kg");
+		lblkg2.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblkg2.setBounds(192, 234, 43, 25);
+		contentPane.add(lblkg2);
+		
+		bCalcularTasaMetabolica = new JButton("Tasa metabólica basal");
+		bCalcularTasaMetabolica.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		bCalcularTasaMetabolica.setBounds(283, 194, 255, 21);
+		contentPane.add(bCalcularTasaMetabolica);
+		
+		tfTasaMetabolica = new JTextField();
+		tfTasaMetabolica.setEditable(false);
+		tfTasaMetabolica.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		tfTasaMetabolica.setBounds(340, 237, 127, 19);
+		contentPane.add(tfTasaMetabolica);
+		tfTasaMetabolica.setColumns(10);
+	}
+	
+	public JTextField getTfAltura() {
+        return tfAltura;
+    }
+
+    public JTextField getTfPeso() {
+        return tfPeso;
+    }
+
+    public JTextField getTfEdad() {
+        return tfEdad;
+    }
+
+    public JTextField getTfPesoIdeal() {
+        return tfPesoIdeal;
+    }
+
+    public JTextField getTfTasaMetabolica() {
+        return tfTasaMetabolica;
+    }
+
+    public JButton getBCalcularPesoIdeal() {
+        return bCalcularPesoIdeal;
+    }
+
+    public JButton getBCalcularTasaMetabolica() {
+        return bCalcularTasaMetabolica;
+    }
+	
+}
