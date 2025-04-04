618e1fc36345a11c1cf7d88a6b2199592c28975a
Lucía Pérez Villegas <luciajmnzvg2003@gmail.com>
2025-04-04 11:57:47 +0200

    Añadida clase Controlador.java

---
+++ project-healthcalc/src/main/java/healthcalc/Controlador.java
@@ -0,0 +1,46 @@
+package healthcalc;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Controlador {
+	
+	private Vista vista;
+	private HealthCalcImpl modelo;
+	
+	// Constructor
+	public Controlador(Vista vista, HealthCalcImpl modelo) {
+		this.vista = vista;
+		this.modelo = modelo;
+		
+		// Calcular el peso ideal
+		vista.getBCalcularPesoIdeal().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					int altura = Integer.parseInt(vista.getTfAltura().getText());
+					char genero = vista.getGeneroSeleccionado();
+					float pesoIdeal = modelo.idealWeight(altura, genero);
+					vista.getTfPesoIdeal().setText(String.format("%.2f", pesoIdeal));
+				} catch (Exception ex) {
+					vista.getTfPesoIdeal().setText(ex.getMessage());
+				}
+			}
+		});
+		
+		// Calcular tasa metabólica basal
+		vista.getBCalcularTasaMetabolica().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					float peso = Float.parseFloat(vista.getTfPeso().getText());
+					int altura = Integer.parseInt(vista.getTfAltura().getText());
+					int edad = Integer.parseInt(vista.getTfEdad().getText());
+					char genero = vista.getGeneroSeleccionado();
+					float tasaMetabolica = modelo.basalMetabolicRate(peso, altura, edad, genero);
+					vista.getTfTasaMetabolica().setText(String.format("%.2f", tasaMetabolica));
+				} catch (Exception ex) {
+					vista.getTfTasaMetabolica().setText(ex.getMessage());
+				}
+			}
+		});	
+	}
+}
